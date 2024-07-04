33693f8d8f43a77c0d42ae3fb3a971a4ac729859
nome <email github>
2024-07-04 14:33:52 -0300

    movies-ok

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Substitua 'SEU_BEARER_TOKEN' pelo seu token de acesso
+// 'BEARER_TOKEN' token de acesso
 const bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjM4OGY3ZjZmOWQ1NmE4YzJjOGY5YzM1NjZhMjZmMSIsIm5iZiI6MTcyMDA1MTM3OS40MTU5MjIsInN1YiI6IjYwMTA1MzczMGYxZTU4MDAzZWFhOGVlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TRdBDVNMBzINQLq8pqqP-WBoZ9HZzctP0Lah_YvomXY';
 
-// Função para carregar os filmes mais populares
+// carregar os filmes mais populares
 function loadPopularMovies() {
     // Selecionar o elemento main
-    var main = document.querySelector('main.center');
+    var main = document.querySelector('.main .center');
 
     // Verificar se o elemento main existe
-    if (main) {
+    if (!main) {
         console.error('Elemento main não encontrado!');
         return;
     }
@@ -24,28 +24,34 @@ function loadPopularMovies() {
     // Realizar a consulta na API do TMDb para obter os filmes mais populares
     fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
     .then(response => response.json())
-    .then(data => {
-        // Verificar se há resultados
-        if (data.results && data.results.length > 0) {
-            // Pegar o primeiro filme da lista (mais popular)
-            const movie = data.results[0];
-
-            // Construir a URL da imagem do filme
-            const imageUrl = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
-
-            // Inserir a imagem no HTML
-            main.innerHTML+= `
-                <div class="banner-principal" style="background-image:url('${imageUrl}')"></div>
-            `;
-        } else {
-            console.error('Nenhum filme encontrado.');
-        }
+    .then(data=>{
+        data.results.map((i,j)=>{
+            if(j > 18)
+                return;
+            if(j == 0){
+                main.innerHTML += `
+                    <div class="banner-principal" style="background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
+                `;
+            } else {
+                if(j == 1){
+                    main.innerHTML +=
+                    ` <h2>Assista os melhores filmes</h2>
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>`
+                }else {
+                    main.innerHTML +=
+                    `
+                    <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>`
+                    
+                }
+            }
+        })
     })
     .catch(err => console.error('Erro ao buscar os filmes:', err));
 }
 
 // Executar a função após o carregamento completo do DOM
 document.addEventListener('DOMContentLoaded', loadPopularMovies);
+
 
 
 
